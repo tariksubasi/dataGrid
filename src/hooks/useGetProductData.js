a889e195b3cb975a3d0de2eb03758afd7ecca985
tarik.subasi <tarik.subasi@hotmail.com>
2022-06-09 15:05:19 +0300

    53 - Adding All Linked Attributes

--- src/hooks/useGetProductData.js
+++ src/hooks/useGetProductData.js
@@ -4,14 +4,24 @@ const useGetProductData = props => {
     let myProductData = [];
 
     // linked attribute to get name attribute of product object inside List<Product>
-    const { productName: prodName } = props;
+    const {
+        productName: prodName,
+        productActive: prodActive,
+        productPrice: prodPrice,
+        productCategory: prodCategory,
+        productQuantity: prodQuantity
+    } = props;
 
     const productItems = props.productData.items;
 
     productItems.forEach(product => {
-        const productName = prodName.get(product).value;
+        const name = prodName.get(product).value;
+        const active = prodActive.get(product).value;
+        const price = parseFloat(prodPrice.get(product).value).toFixed(2);
+        const category = prodCategory.get(product).value;
+        const quantity = parseInt(prodQuantity.get(product).value);
 
-        myProductData.push({ productName });
+        myProductData.push({ name, active, price, category, quantity });
     });
 
     return { myProductData };
